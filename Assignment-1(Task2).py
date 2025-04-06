f7f2ab114f16258e2bbbb8bcd3cb4c158b8f654a
Anupto <anoop.savant@gmail.com>
2025-04-06 18:19:22 +0530

    Add files via upload

---
+++ Assignment-1(Task2).py
@@ -0,0 +1,10 @@
+#task 2
+FName = input('Enter your First Name: ')
+LName = input('Enter your Last Name: ')
+
+FName = str(FName)
+LName = str(LName)
+
+FullName = FName + ' ' + LName + '!'
+
+print('Hello,' ,FullName,'Welcome to Python Program.')
---
+++ Assignment-1.py
@@ -0,0 +1,16 @@
+#Task 1
+num1 = input('Enter the First number: ')
+num2 = input('Enter the second number: ')
+
+num1 = int(num1)
+num2 = int(num2)
+
+Add = num1 + num2
+Sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print('Addition: ',Add)
+print('Subtraction: ',Sub)
+print('Multiplication: ',mul)
+print('Division: ',div)
